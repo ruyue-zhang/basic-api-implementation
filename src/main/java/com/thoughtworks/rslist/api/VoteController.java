cdfdcbc6c70191f6ae9b75820a594b47114c95a2
lpdft <772346730@qq.com>
2020-09-19 13:33:35 +0800

    [ruyue] feat:implement return vote records between start and end time

--- src/main/java/com/thoughtworks/rslist/api/VoteController.java
+++ src/main/java/com/thoughtworks/rslist/api/VoteController.java
@@ -1,8 +1,40 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.dto.Vote;
+import com.thoughtworks.rslist.repository.VoteRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class VoteController {
+    final VoteRepository voteRepository;
 
+    public VoteController(VoteRepository voteRepository) {
+        this.voteRepository = voteRepository;
+    }
+
+    @ResponseStatus(code = HttpStatus.OK)
+    @GetMapping("/votes")
+    public List<Vote> getVotes(@RequestParam(required = false) String start,
+                               @RequestParam(required = false) String end) {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime startTime = LocalDateTime.parse(start,df);
+        LocalDateTime endTime = LocalDateTime.parse(start,df);
+
+        return voteRepository.findByVoteTimeBetween(startTime, endTime).stream().map(voteEntity -> Vote.builder()
+                    .voteNum(voteEntity.getVoteNum())
+                .voteTime(voteEntity.getVoteTime())
+                .userId(voteEntity.getUserEntity().getId())
+                .rsEventId(voteEntity.getRsEventEntity().getId())
+                .build()
+        ).collect(Collectors.toList());
+    }
 }
--- src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -1,14 +1,71 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.entity.RsEventEntity;
+import com.thoughtworks.rslist.entity.UserEntity;
+import com.thoughtworks.rslist.entity.VoteEntity;
+import com.thoughtworks.rslist.repository.RsEventRepository;
+import com.thoughtworks.rslist.repository.UserRepository;
+import com.thoughtworks.rslist.repository.VoteRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class VoteControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RsEventRepository rsEventRepository;
+    @Autowired
+    VoteRepository voteRepository;
 
     @Test
-    void should_return_vote_record_between_start_and_end_time() {
+    void should_return_vote_record_between_start_and_end_time() throws Exception {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String start = df.format(LocalDateTime.now());
 
+        UserEntity userEntity = UserEntity.builder()
+                .name("zhangSan")
+                .gender("female")
+                .age(25)
+                .email("666@twuc.com")
+                .phone("18800000000")
+                .vote(10)
+                .build();
+        userRepository.save(userEntity);
+
+        RsEventEntity rsEventEntity = RsEventEntity.builder()
+                .eventName("猪肉涨价了")
+                .keyWord("经济")
+                .userId(userEntity.getId())
+                .build();
+        rsEventRepository.save(rsEventEntity);
+        VoteEntity voteEntity = VoteEntity.builder()
+                .userEntity(userEntity)
+                .rsEventEntity(rsEventEntity)
+                .voteNum(1)
+                .voteTime(LocalDateTime.now())
+                .build();
+        voteRepository.save(voteEntity);
+
+        String end = df.format(LocalDateTime.now());
+        mockMvc.perform(get("/votes?start={start}&end={end}",start, end))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].userId", is(userEntity.getId())))
+                .andExpect(jsonPath("$[0].rsEventId", is(rsEventEntity.getId())))
+                .andExpect(jsonPath("$[0].voteNum", is(1)));
     }
-
 }
